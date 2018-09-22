4dedb80791ec0f6d6da54162f754a36be286223a
Dan <dlaubacher3@gmail.com>
2018-09-22 14:16:53 -0400

    build out toodle model

--- models.toodles.go
+++ models.toodles.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/globalsign/mgo/bson"
+import (
+	"github.com/globalsign/mgo/bson"
+	mgo "gopkg.in/mgo.v2"
+)
 
 // Toodle model
 type Toodle struct {
 	ID      bson.ObjectId `bson:"_id" form:"id" json:"id" `
 	Title   string        `bson:"title" form:"title" json:"title" binding:"required,max=150"`
-	Content string        `bson:"content" form:"content" json:"content"`
+	Content string        `bson:"content" form:"content" json:"content" binding:"max=2000"`
 	Status  string        `bson:"status" form:"status" json:"status"`
 }
 
@@ -14,4 +17,32 @@ type Toodle struct {
 type Toodles struct {
 	ID      bson.ObjectId `bson:"_id" form:"id" json:"id" `
 	Toodles []Toodle      `bson:"toodles"`
+}
+
+func getToodles(UID string, toodles *Toodles) error {
+	return mongo.C(collectionToodles).FindId(bson.ObjectIdHex(UID)).One(&toodles)
+}
+
+func createToodle(UID string, toodle *Toodle) (*mgo.ChangeInfo, error) {
+	query := bson.M{"_id": bson.ObjectIdHex(UID)}
+	update := bson.M{"$push": bson.M{"toodles": &toodle}}
+	return mongo.C(collectionToodles).Upsert(query, update)
+}
+
+func completeToodle(UID string, id string) error {
+	query := bson.M{"_id": bson.ObjectIdHex(UID), "toodles._id": bson.ObjectIdHex(id)}
+	update := bson.M{"$set": bson.M{"toodles.$.status": "complete"}}
+	return mongo.C(collectionToodles).Update(query, update)
+}
+
+func updateToodle(UID string, id string, toodle *Toodle) error {
+	query := bson.M{"_id": bson.ObjectIdHex(UID), "toodles._id": bson.ObjectIdHex(id)}
+	update := bson.M{"$set": bson.M{"toodles.$": &toodle}}
+	return mongo.C(collectionToodles).Update(query, update)
+}
+
+func deleteToodle(UID string, id string) (*mgo.ChangeInfo, error) {
+	query := bson.M{"_id": bson.ObjectIdHex(UID)}
+	update := bson.M{"$pull": bson.M{"toodles": bson.M{"_id": bson.ObjectIdHex(id)}}}
+	return mongo.C(collectionToodles).Upsert(query, update)
 }
